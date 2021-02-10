dda8634ce017a1bdbed5422f9e3dc54f60d38cfb
Denz-afk <78831788+Denz-afk@users.noreply.github.com>
2021-02-09 21:35:08 -0400

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace b3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32 debm;
+            debm = 10*10;
+            do{
+                debm = debm+2;
+                Console.WriteLine(""+debm);
+            }while (debm<121);
+            Console.ReadKey();
+        }
+    }
+}
